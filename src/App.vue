e4ec21672f6982471fcf46bfe06ec1f5bd60467c
SpilmontFrancois <francois.spilmont5@etu.univ-lorraine.fr>
2021-12-13 17:59:39 +0100

    feat(app): add rainbow animation

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
-    <button @click="setRandomColor">Click ME</button>
+    <button class="btn btn-primary" @click="setRandomColor">
+      Random color
+    </button>
+    <button class="btn btn-primary" @click="playRainbow">Rainbow</button>
   </div>
 </template>
 
@@ -9,6 +12,7 @@ export default {
   data() {
     return {
       lightsIDS: [1, 2],
+      stop: false,
     }
   },
   methods: {
@@ -42,6 +46,24 @@ export default {
         this.controlLight(lightID, true, hue, sat, bri)
       })
     },
+
+    async playRainbow() {
+      let hue = 0
+      const sat = 200
+      const bri = 255
+      while (!this.stop) {
+        while (hue < 65535) {
+          this.lightsIDS.forEach((lightID) => {
+            this.controlLight(lightID, true, hue, sat, bri)
+          })
+          hue += 500
+          await new Promise((resolve) => setTimeout(resolve, 100))
+        }
+        hue = 0
+      }
+    },
   },
 }
+// TODO : dispay the state and the color of each light => http://192.168.1.89/api/Vh1L2FGD49gH7tflufC2R36GQq3kGq0XZJE5zbpe
+// TODO : add a control panel to control all lights
 </script>
